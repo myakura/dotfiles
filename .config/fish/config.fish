82da7f91d67cddafb42ac9b3cc416a10d565cf7f
Masataka Yakura <masataka.yakura@gmail.com>
2019-02-20 23:05:54 +0900

    reflect what I use and do not use lately

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,31 +3,21 @@ set fish_greeting ""
 
 # PATH
 set --export PATH $HOME/.nodebrew/current/bin $PATH
-set --export PATH $HOME/.cargo/bin $PATH
-set --export PATH $HOME/tools/flutter/bin $PATH
 
 # functions
 function reload-config ; source ~/.config/fish/config.fish ; end
 
-# cask
-set --export HOMEBREW_CASK_OPTS "--appdir=/Applications"
-
 # Apps
 alias g      "git"
-alias server "python -m SimpleHTTPServer"
+alias server "python3 -m http.server"
 
 # File and directory
-alias c     "clear"
 alias u     "cd $HOME/"
 alias p     "cd $HOME/projects/"
 alias cp    "cp -i"
-alias up    "cd .."
-alias trash "rmtrash"
-alias cpdir "cp -pR"
 
 # Homebrew
-alias brewup "brew update; and brew upgrade; and brew cleanup; and brew prune"
+alias brewup "brew update; and brew upgrade"
 
 # Git
-alias pull  "git pull"
 alias fumum "git fetch upstream; and git merge upstream/master"
