6c3f695cec0983245d86fc4b34fb66e502c6a406
Masataka Yakura <masataka.yakura@gmail.com>
2020-08-10 13:41:05 +0900

    fish: add scaffolding

--- fish/config.fish
+++ fish/config.fish
@@ -26,3 +26,42 @@ abbr --add --global aptup "sudo apt update && sudo apt upgrade"
 
 # git
 abbr --add --global fumum "git fetch upstream && git merge upstream/master"
+
+# scaffolding
+function scaffold -d "create a project with a bit of initialization"
+  set argcount (count $argv)
+  echo "scaffold called with $argcount args: $argv"
+
+	# if no arguments given, use the current directory
+	set working_directory (pwd)
+
+  # grab the last argument as a name of the project
+  if test $argcount -gt 0
+    set name $argv[-1]
+		echo "name: $name"
+
+		set working_directory (pwd)"/"$name
+    mkdir -p $working_directory
+		cd $working_directory
+  end
+
+	echo "directory: $working_directory"
+
+	echo "set up editorconfig and prettier"
+  cp ~/.dotfiles/.editorconfig .
+  cp ~/.dotfiles/.prettierrc.toml .
+
+	# treat arguments besides the last one as options
+	if test $argcount -gt 1
+		set options $argv[1..-2]
+		echo "options: $options"
+
+		if contains -- "--git" $options
+			git init
+		end
+
+		if contains -- "--npm" $options
+			npm init --yes
+		end
+	end
+end
